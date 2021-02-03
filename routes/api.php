a1a11e33525f0fb6f60104db6636376e194d3401
h.kato <h.kato@cocolabo.jp>
2021-02-04 01:51:01 +0900

    testing grant type client

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,11 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::middleware('laravel.passport.client')->get('/client1', function (Request $request) {
+    return $request->user();
+});
+Route::middleware('laravel.passport.client')->get('/client2', function (Request $request) {
+    return response('auth client for laravel passport');
+});
+
+//
