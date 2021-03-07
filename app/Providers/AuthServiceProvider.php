563b15f370faf7af41bc508ceb8ad99e435e13ae
h.kato <h.kato@cocolabo.jp>
2021-03-08 00:15:17 +0900

    有効期限設定

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Gate;
 use Laravel\Passport\Passport;
 
@@ -33,5 +34,8 @@ class AuthServiceProvider extends ServiceProvider
             $router->forClients();
 //            $router->forPersonalAccessTokens();
         });
+
+        Passport::tokensExpireIn(Carbon::now()->addMinutes(1));
+        Passport::refreshTokensExpireIn(Carbon::now()->addMinutes(10));
     }
 }
